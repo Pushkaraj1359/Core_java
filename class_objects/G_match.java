8d42c01389ecb3b2ce4f345476b9ffd2cc60d2fc
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2026-01-08 20:18:36 +0530

    add one method as team

---
+++ class_objects/G_match.java
@@ -0,0 +1,68 @@
+class Player{
+    String name;
+    int runs;
+
+    Player(String name){
+        this.name=name;
+        this.runs=0;
+    }
+
+    void addRuns(int r){
+        runs +=r;
+    }
+}
+
+class Team {
+    String name;
+    int totalRuns;
+    int wickets;
+    int overs;
+
+    Team(String name){
+        this.name = name;
+        totalRuns = 0;
+        wickets = 0;
+        overs = 0;
+    }
+
+    void addRuns(int runs){
+        totalRuns += runs;
+    }
+
+    void addWicket(){
+        wickets++;
+    }
+
+    void addOver(){
+        overs++;
+    }
+}
+
+class ScoreBoard {
+
+    void displayScore(Team team) {
+        System.out.println("Team: " + team.name);
+        System.out.println("Score: " + team.totalRuns + "/" + team.wickets);
+        System.out.println("Overs: " + team.overs);
+        System.out.println("----------------------");
+    }
+}
+public class G_match {
+    public static void main(String[] args) {
+        Team india = new Team("India");
+        ScoreBoard board = new ScoreBoard();
+
+    
+        india.addRuns(4);
+        india.addRuns(2);
+        india.addWicket();
+        india.addOver();
+
+        board.displayScore(india);
+
+        india.addRuns(6);
+        india.addRuns(1);
+
+        board.displayScore(india);
+    }
+}
