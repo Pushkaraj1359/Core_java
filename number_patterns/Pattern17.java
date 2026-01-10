ff4639467c812aba97fb795d04aad4797fa72431
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2026-01-10 17:11:44 +0530

    number patterns logical examples added

---
+++ number_patterns/Pattern17.java
@@ -0,0 +1,23 @@
+// 54321
+//  4321
+//   321
+//    21
+//     1
+
+package number_patterns;
+
+public class Pattern17 {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=n,k=n;i>=1;i--,k--){
+            int p=k;
+            for(int space=1;space<=n-i;space++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(p--);
+            }
+            System.out.println();
+        }
+    }
+}
