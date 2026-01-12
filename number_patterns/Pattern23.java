b7c7daebd0a4adef195b061376d631c6e458c18b
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2026-01-12 19:42:00 +0530

    some updates in number pattern file

--- number_patterns/Pattern23.java
+++ number_patterns/Pattern23.java
@@ -1,23 +1,13 @@
+// 1 
+// 2 6 
+// 3 7 10 
+// 4 8 11 13 
+// 5 9 12 14 15
+
 package number_patterns;
 
 public class Pattern23 {
     public static void main(String[] args) {
-        // int n = 5;
-        // int count = 1;
-        // int value = n-1;
-        // for(int i=1;i<=n;i++){
-        //     int c = count;
-        //     for(int j=1;j<=i;j++){
-        //         System.out.print(c+" ");
-        //         c = c + value;
-        //         value--;
-        //     }
-        //     value = n-1;
-        //     count++;
-        //     System.out.println();
-        // }
-
-
         int n = 5;
         int count = 1;
         int value = n-1;
@@ -28,24 +18,9 @@ public class Pattern23 {
                 c = c + value;
                 value--;
             }
-            value=n-1;
+            value = n-1;
             count++;
             System.out.println();
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
