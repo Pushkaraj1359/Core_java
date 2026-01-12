a7df0de41f8728beee3c6971669b10b874cca2d8
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2026-01-12 19:37:19 +0530

    number patterns with row and colomn gap

---
+++ number_patterns/Pattern22.java
@@ -0,0 +1,26 @@
+// 15
+// 13      14
+// 10      11      12
+// 6       7       8       9
+// 1       2       3       4       5
+
+package number_patterns;
+
+public class Pattern22 {
+    public static void main(String[] args) {
+        int n = 5;
+        int count = 15;
+        int value = 2;
+        for(int i=1;i<=n;i++){
+            int c = count;
+            for(int j=1;j<=i;j++){
+                System.out.print(c+"\t");
+                c++;
+            }
+            count = count -value;
+            value++;
+            
+            System.out.println();
+        }
+    }
+}
---
+++ number_patterns/Pattern23.java
@@ -0,0 +1,51 @@
+package number_patterns;
+
+public class Pattern23 {
+    public static void main(String[] args) {
+        // int n = 5;
+        // int count = 1;
+        // int value = n-1;
+        // for(int i=1;i<=n;i++){
+        //     int c = count;
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(c+" ");
+        //         c = c + value;
+        //         value--;
+        //     }
+        //     value = n-1;
+        //     count++;
+        //     System.out.println();
+        // }
+
+
+        int n = 5;
+        int count = 1;
+        int value = n-1;
+        for(int i=1;i<=n;i++){
+            int c = count;
+            for(int j=1;j<=i;j++){
+                System.out.print(c+" ");
+                c = c + value;
+                value--;
+            }
+            value=n-1;
+            count++;
+            System.out.println();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
