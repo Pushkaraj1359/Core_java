6481eea2640ccc36aa1ede50565ae5f0e2d170a0
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2026-01-12 18:36:11 +0530

    class calling without creating object

--- number_patterns/Pattern18.java
+++ number_patterns/Pattern18.java
@@ -4,17 +4,40 @@
 // 4 8 11 13
 // 5 9 12 14 15
 
+// package number_patterns;
+// public class Pattern18 {
+//     public static void main(String[] args){
+//         int n=5;
+//         for(int i=1;i<=n;i++){
+//             int p = i;
+//             for(int j=1;j<=i;j++){
+//                 System.out.print(p+" ");
+//                 p=p+n-j;
+//             }
+//             System.out.println();
+//         }
+//     }   
+// }
+
+
+// 1 
+// 3 2
+// 6 5 4
+// 10 9 8 7
+// 15 14 13 12 11
+
 package number_patterns;
 public class Pattern18 {
     public static void main(String[] args){
         int n=5;
+        int count = 0;
         for(int i=1;i<=n;i++){
-            int p = i;
+            count = count + i;
+            int temp=count;
             for(int j=1;j<=i;j++){
-                System.out.print(p+" ");
-                p=p+n-j;
+                System.out.print(temp--);
             }
             System.out.println();
         }
-    }   
+    }
 }
