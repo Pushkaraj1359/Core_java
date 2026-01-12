d04e665be68b1922da9cb42cac03fefbb183d6d1
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2026-01-12 18:52:07 +0530

    added one more pattern to build logic

---
+++ number_patterns/Pattern20.java
@@ -0,0 +1,12 @@
+package number_patterns;
+
+public class Pattern20 {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
