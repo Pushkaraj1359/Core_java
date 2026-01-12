2250dc6c12590aede3b9ef432a5baebe06130173
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2026-01-12 19:01:19 +0530

    pattern added >>>21

---
+++ number_patterns/Pattern21.java
@@ -0,0 +1,23 @@
+// 1
+// 3       2
+// 6       5       4
+// 10      9       8       7
+// 15      14      13      12      11
+
+package number_patterns;
+
+public class Pattern21 {
+    public static void main(String[] args) {
+        int n = 5;
+        int count = 1;
+        for(int i=1;i<=n;i++){
+            int c = count;
+            for(int j=1;j<=i;j++){
+                System.out.print(c+"\t");
+                c--;
+            }
+            count = count + i + 1;
+            System.out.println();
+        }
+    }
+}
