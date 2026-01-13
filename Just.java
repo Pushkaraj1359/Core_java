7a52d501f11ad4c919e4af34a7adfc63b4cb4b37
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2026-01-13 21:53:50 +0530

    add pattern code

--- Just.java
+++ Just.java
@@ -220,26 +220,144 @@
 // }
 
 
+
+// 1
+// 21
+// 321
+// 4321
+// 54321
+// 4321
+// 321
+// 21
+// 1
+// public class Just { 
+//     public static void main(String[] args){
+//         int n=5;
+//         for( int i=1;i<=(n*2)-1;i++){
+//             if (i<=n) {
+//                 int c = i;
+//                 for(int j=1;j<=i;j++){
+//                     System.out.print(c);
+//                     c--;
+//                 }
+//             }
+//             else{
+//                 int c = (n*2)-i;
+//                 for( int j=c;j>=1;j--){
+//                     System.out.print(c);
+//                     c--;
+//                 }
+//             }
+//         System.out.println();
+//         }
+//     }
+// }
+
+
+
+// 5
+// 54
+// 543
+// 5432
+// 54321
+// 5432
+// 543
+// 54
+// 5
+// public class Just { 
+//     public static void main(String[] args){
+//         int n =5;
+//         for(int i=1;i<=(n*2)-1;i++){
+//             if(i<=n){
+//                 int c = n;
+//                 for(int j=1;j<=i;j++){
+//                     System.out.print(c);
+//                     c--;
+//                 }
+//             }
+//             else{
+//                 int b = n;
+//                 for(int j=(n*2)-i;j>=1;j--){
+//                     System.out.print(b);
+//                     b--;
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+
+
+// 5
+// 45
+// 345
+// 2345
+// 12345
+// 2345
+// 345
+// 45
+// 5
+// public class Just { 
+//     public static void main(String[] args){
+        
+//         int n = 5;
+//         for(int i=1;i<=(2*n)-1;i++){
+//             if(i<=n){
+//                 int c = n-i+1;
+//                 for(int j=1;j<=i;j++){
+//                     System.out.print(c);
+//                     c++;
+//                 }
+//             }
+//             else{
+//                 int b = i - n + 1;
+//                 for(int j=1;j<=(2*n)-i;j++){
+//                     System.out.print(b);
+//                     b++;
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
 public class Just { 
     public static void main(String[] args){
-        int n=5;
-        for( int i=1;i<=(n*2)-1;i++){
-            if (i<=n) {
+        int n = 5;
+        for(int i=1;i<=(2*n)-1;i++){
+            if(i<=n){
                 for(int j=1;j<=i;j++){
-                    System.out.print(i);
+                    if(i%2!=0){
+                        System.out.print((char)(i+64));
+                    }
+                    else{
+                        System.out.print((char)(i+96));
+                    }
                 }
+                System.out.println();
             }
             else{
-                int c = (n*2)-i;
-                for( int j=c;j>=1;j--){
-                    System.out.print(c);
-                    c--;
+                int c = (2*n)-i;
+                for(int j=1;j<=(n*2)-i;j++){
+                    if(i%2!=0){
+                        System.out.print((char)(c+64));
+                    }
+                    else{
+                        System.out.print((char)(c+96));
+                    }
                 }
+                System.out.println();
             }
-        System.out.println();
         }
     }
 }
+
+
 
 
 
