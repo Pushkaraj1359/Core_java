f2a474258a33a0c18fb3f8492a4a40594e915afa
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2026-01-13 19:10:03 +0530

    add number patterns

--- Just.java
+++ Just.java
@@ -159,6 +159,85 @@
 
 
 
+// *
+// **
+// ***
+// ****
+// *****
+// ****
+// ***
+// **
+// *
+// public class Just { 
+//     public static void main(String[] args){
+//         int n = 5;
+//         for(int i=1;i<=n*2-1;i++){
+//             if(i<=n){
+//                 for(int j=1;j<=i;j++){
+//                     System.out.print("*");
+//                 }
+//             }
+//             else{
+//                 int c = (2*n)-i;
+//                 for(int j=c;j>=1;j--){
+//                     System.out.print("*");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+// 1
+// 22
+// 333
+// 4444
+// 55555
+// 4444
+// 333
+// 22
+// 1
+// public class Just { 
+//     public static void main(String[] args){
+//         int n =5;
+//         for(int i=1;i<=(2*n)-1;i++){
+//             if(i<=n){
+//                 for(int j=1;j<=i;j++){
+//                     System.out.print(i);
+//                 }
+//             }
+//             else{
+//                 int c = (n*2)-i;
+//                 for(int j=c;j>=1;j--){
+//                     System.out.print(c);
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+public class Just { 
+    public static void main(String[] args){
+        int n=5;
+        for( int i=1;i<=(n*2)-1;i++){
+            if (true) {
+                
+            }
+            else{
+                
+            }
+        }
+    }
+}
+
+
+
+
+
 
 
 
--- demo.java
+++ demo.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
 public class demo {
     public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Nums");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
         
-    }
+        int i = 1;
+        int hcf = 1;
+
+        while(i<=a && i<=b){
+            if (a%i==0 && b%i==0) {
+                hcf = i;           
+            }
+
+            i++;
+        }
+
+        System.out.println("Hcf of" +a+"&"+b+":"+hcf);
+
+        int lcm = (a*b)/hcf;
+        System.out.println("Lcm of" +a+"&"+b+":"+lcm);
+        
+        }
 }
