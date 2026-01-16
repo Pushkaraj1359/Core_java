9937bcd3f5ca50ff96811b50fd4d59e71a229e30
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2026-01-16 19:37:21 +0530

    add pattern

--- number_patterns/Pattern15.java
+++ number_patterns/Pattern15.java
@@ -8,10 +8,25 @@ package number_patterns;
 
 public class Pattern15 {
     public static void main(String[] args) {
-        int n=5;
-        for(int i=1;i<=5;i++){
-            int p=1;
-            // print space
+        // int n=5;
+        // for(int i=1;i<=5;i++){
+        //     int p=1;
+        //     // print space
+        //     for(int space=1;space<=n-i;space++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(p++);
+        //     }
+        //     for(int j=1;j<i;j++){
+        //         System.out.print(p++);
+        //     }
+        //     System.out.println();
+        // }
+
+        int n = 5;
+        for(int i=1;i<=n;i++){
+            int p =1;
             for(int space=1;space<=n-i;space++){
                 System.out.print(" ");
             }
