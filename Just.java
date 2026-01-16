33973c773bda4f53d64b55589fe90d3143711e65
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2026-01-16 17:57:57 +0530

    patterns

--- Just.java
+++ Just.java
@@ -336,32 +336,62 @@
 // bb
 // A
 
+// public class Just { 
+//     public static void main(String[] args){
+//         int n = 5;
+//         for(int i=1;i<=(2*n)-1;i++){
+//             if(i<=n){
+//                 for(int j=1;j<=i;j++){
+//                     if(i%2!=0){
+//                         System.out.print((char)(i+64));
+//                     }
+//                     else{
+//                         System.out.print((char)(i+96));
+//                     }
+//                 }
+//                 System.out.println();
+//             }
+//             else{
+//                 int c = (2*n)-i;
+//                 for(int j=1;j<=(n*2)-i;j++){
+//                     if(i%2!=0){
+//                         System.out.print((char)(c+64));
+//                     }
+//                     else{
+//                         System.out.print((char)(c+96));
+//                     }
+//                 }
+//                 System.out.println();
+//             }
+//         }
+//     }
+// }
+
+
 public class Just { 
     public static void main(String[] args){
         int n = 5;
-        for(int i=1;i<=(2*n)-1;i++){
+        for(int i=1;i<=(n*2)-1;i++){
             if(i<=n){
                 for(int j=1;j<=i;j++){
-                    if(i%2!=0){
-                        System.out.print((char)(i+64));
+                    if(j%2==0){
+                        System.out.print((char)(j+96));
                     }
                     else{
-                        System.out.print((char)(i+96));
+                        System.out.print((char)(j+64));
                     }
                 }
                 System.out.println();
             }
             else{
-                int c = (2*n)-i;
-                for(int j=1;j<=(n*2)-i;j++){
-                    if(i%2!=0){
-                        System.out.print((char)(c+64));
+                for(int j=(2*n)-i;j>=1;j--){
+                    if(j%2==0){
+                        System.out.print((char)(j+96));
                     }
                     else{
-                        System.out.print((char)(c+96));
+                        System.out.print((char)(j+46));
                     }
                 }
-                System.out.println();
             }
         }
     }
