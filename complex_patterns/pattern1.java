608848e85cef6dba184b5c91ed37825579faa5b3
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2026-01-17 14:00:07 +0530

    add pattern_imp

--- complex_patterns/pattern1.java
+++ complex_patterns/pattern1.java
@@ -3,14 +3,28 @@ package complex_patterns;
 public class pattern1 {
     public static void main(String[] args) {
         int n =5;
-        for(int k=1;k<=n;k++){
+        for(int l=1;l<=n;l++){
+            for(int k=1;k<=n;k++){
             for(int i=1;i<=n;i++){
                 for(int j=1;j<=n;j++){
-                    System.out.print("*");
+                    if(l%2!=0 && i%2==0  || l%2==0 && i%2==1){
+                        System.out.print(" ");
+                    }
+                    else{
+                        System.out.print("*");
+                    }
+                    // if(l%2!=0 && i%2==0){
+                    //     System.out.print("*");
+                    // }
+                    // else{
+                    //     System.out.print("*");
+                    // }
                 }
-                System.out.print("_");
+                System.out.print(" ");
             }
             System.out.println();
+        }
+        System.out.println();
         }
     }
 }
---
+++ complex_patterns/pattern_imp.java
@@ -0,0 +1,7 @@
+package complex_patterns;
+
+public class pattern_imp {
+    public static void main(String[] args) {
+        
+    }
+}
