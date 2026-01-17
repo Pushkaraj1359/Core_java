9c19d927109bc70497f2a33bc52e5e61d29f3307
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2026-01-17 14:05:08 +0530

    complex pattern imp

--- complex_patterns/pattern_imp.java
+++ complex_patterns/pattern_imp.java
@@ -2,6 +2,11 @@ package complex_patterns;
 
 public class pattern_imp {
     public static void main(String[] args) {
-        
+        int num = 4;
+        for(int n=0;n<=n;n++){
+            for(int k=0;k<=n;k++){
+
+            }
+        }
     }
 }
