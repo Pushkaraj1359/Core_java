4268f52b3460ac2e159708aaf6d271ca16add30c
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2026-01-17 14:59:25 +0530

    add mostly asked interview question

--- text.java
+++ text.java
@@ -1,10 +1,9 @@
 /* 
+
+>>>>>>>>----------MOSTLY ASKED INTERVIEW QUESTION---------->>>>>>>>
+
 [ ]   -------> subscript
 int arr[]=new int[5] {11,22,33,44,55}; --->>>>>>compile time error;
-
-
-
-
 
 
 
