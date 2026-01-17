ac6314973468d2b80e7a58f6613a6f64538a6ccc
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2026-01-17 13:29:19 +0530

    add complex pattern folder

---
+++ complex_patterns/pattern1.java
@@ -0,0 +1,16 @@
+package complex_patterns;
+
+public class pattern1 {
+    public static void main(String[] args) {
+        int n =5;
+        for(int k=1;k<=n;k++){
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=n;j++){
+                    System.out.print("*");
+                }
+                System.out.print("_");
+            }
+            System.out.println();
+        }
+    }
+}
