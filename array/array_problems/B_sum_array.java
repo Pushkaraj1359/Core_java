c15f28bbf76ac3d2abf7a6d79b53d982020d9d46
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2026-01-20 13:04:57 +0530

    add array priblem like forward , backword , odd elements sum and so much

--- array/array_problems/B_sum_array.java
+++ array/array_problems/B_sum_array.java
@@ -11,27 +11,36 @@ public class B_sum_array {
         System.out.println();
 
         // backward element printing 
-        // for(int i=arr.length-1;i>=0;i--){
-        //     System.out.print(arr[i]+" ");
-        // }
-        // System.out.println();
+        for(int i=arr.length-1;i>=0;i--){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
 
-        // int sum1= 0;
-        // for(int i=0;i<arr.length;i++){
-        //     if(arr[i]%2!=0){
-        //         sum1=sum1+arr[i];
-        //     }
-        // }
-        // System.out.println("the sum of odd elements = "+ sum1);
+        // Sum of odd elements
+        int sum1= 0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%2!=0){
+                sum1=sum1+arr[i];
+            }
+        }
+        System.out.println("the sum of odd elements = "+ sum1);
 
-        // int sum = 0;
-        // for(int i=0;i<arr.length;i++){
-        //     sum = sum +arr[i];
-        // }
-        // System.out.println("sum of array = "+sum);
+        // Sum of all elements
+        int sum = 0;
+        for(int i=0;i<arr.length;i++){
+            sum = sum +arr[i];
+        }
+        System.out.println("sum of array = "+sum);
 
-        // float average;
-        // average = sum / arr.length;
-        // System.out.println("the array average is = "+average);
+        float average;
+        average = sum / arr.length;        // average calculation
+        System.out.println("the array average is = "+average);
+
+        // print element value is greater then average
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>average){
+                System.out.println(arr[i]);
+            }
+        }
     }
 }
