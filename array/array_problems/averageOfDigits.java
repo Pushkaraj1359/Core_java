b3bbc0ea9f2d4926ee4f575ec5b26b7941a90de9
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2026-01-22 14:40:00 +0530

    add array problems with solutions

--- array/array_problems/averageOfDigits.java
+++ array/array_problems/averageOfDigits.java
@@ -10,23 +10,38 @@
 package array_problems;
 
 public class averageOfDigits {
-    static void findDigitAve(int A[]){
-        for(int i=0;i<A.length;i++){
-            int sum = 0;
-            int count = 0;
-            while(A[i]>0){
-                int temp = A[i] % 10;
-                sum = sum + temp;
-                A[i] = A[i] / 10;
-                count++;
-            }
-            System.out.println("sum"+sum+ " "+"count of digit"+count );
-            // int average = sum / count;
-            int ave =sum/count;
-            System.out.println("average = "+ave);
-            // A[i]=ave;
+    // static void findDigitAve(int A[]){
+    //     for(int i=0;i<A.length;i++){
+    //         int sum = 0;
+    //         int count = 0;
+    //         while(A[i]>0){
+    //             int temp = A[i] % 10;
+    //             sum = sum + temp;
+    //             A[i] = A[i] / 10;
+    //             count++;
+    //         }
+    //         System.out.println("sum"+sum+ " "+"count of digit"+count );
+    //         // int average = sum / count;
+    //         int ave =sum/count;
+    //         System.out.println("average = "+ave);
+    //         // A[i]=ave;
+    //     }
+    // }
+
+    static int sumDigit(int num){
+        int sum = 0;
+        while(num!=0){
+            int temp = num % 10;
+            sum = sum + temp;
+            num = num / 10;
         }
+        retun sum;
     }
+    // static void averageDigit(int A[]){
+    //     for(int i=0;i<A.length;i++){
+    //         A[i]= sumOfDigit(A[i])/countDigit(A[i]);
+    //     }
+    // }
     public static void main(String[] args){
         int A[]={123,235,6754,3257,64,2367,288,35,645,374,56};
         findDigitAve(A);
