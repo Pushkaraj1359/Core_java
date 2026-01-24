bdb8c73c3f62537a828f9c10142901ed256443b3
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2026-01-24 15:27:29 +0530

    array problems and solutions

--- array/array_problemsAndSolutions/Dynamic_array_create.java
+++ array/array_problemsAndSolutions/Dynamic_array_create.java
@@ -1,4 +1,10 @@
-package array_practice;
+//  Array Case 1: Forward and Backward Display
+//  Create an array of integers, store elements in it, and display the elements:
+// - In forward order (from first to last)
+// - In backward order (from last to first)
+
+
+package array_problemsAndSolutions;
 import java.util.Scanner;
 
 public class Dynamic_array_create {
@@ -11,12 +17,16 @@ public class Dynamic_array_create {
         for(int i=0;i<A.length;i++){
             A[i]=scanner.nextInt();
         }
-
+        System.out.print("array elements : ");
         for(int i=0;i<A.length;i++){
             System.out.print(A[i]+" ");
         }
+        System.out.println();
     }
 
+
+
+    // Backward Array
     static void backwardArray(int A[]){
         System.out.print("backword array : ");
         for(int i=A.length-1;i>=0;i--){
@@ -27,6 +37,7 @@ public class Dynamic_array_create {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("enter array size");
         int size = scanner.nextInt();
         createArray(size);
 
