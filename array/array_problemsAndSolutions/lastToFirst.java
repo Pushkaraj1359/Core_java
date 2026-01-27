5b08f011eb926fc8ec23ad774f044415735cddb0
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2026-01-27 08:58:05 +0530

    array swap problems

---
+++ array/array_problemsAndSolutions/lastToFirst.java
@@ -0,0 +1,37 @@
+package array_problemsAndSolutions;
+import java.util.Scanner;
+
+public class lastToFirst {
+    static void createArray(int size){
+        Scanner scanner = new Scanner(System.in);
+        int A[] = new int[size];
+        // take array element as an input 
+        for(int i=0,count=0;i<A.length;i++,count++){
+            System.out.print("enter array " + count + " element ");
+            A[i]=scanner.nextInt();
+        }
+        // display array elements
+        System.out.print("display array elements : ");
+        for(int i=0;i<A.length;i++){
+            System.out.print(A[i]+" ");
+        }
+
+        int lastElement = A[A.length-1];
+        for(int i=A.length-1;i>0;i--){
+            A[i]=A[i-1];
+        }
+        A[0]=lastElement;
+        System.out.println();
+        System.out.print("the updated array elements : ");
+        for(int i=0;i<A.length;i++){
+            System.out.print(A[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("please enter array size");
+        int size = scanner.nextInt();
+        createArray(size);
+    }
+}
