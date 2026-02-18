98fce1bb88779c0d5e6b2e17ccceb4eb989b7bb6
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2026-02-18 11:26:08 +0530

    default data type values by using function calling

---
+++ class_objects/classes/DataType.java
@@ -0,0 +1,36 @@
+package class_objects.classes;
+
+class Data{
+	boolean is;
+	byte bt;
+	char letter;
+	short sh;
+	int in;
+	long lo;
+	float fl;
+	double dl;
+	String s;
+
+    void displayData(){
+        System.out.println("Boolean  "+is);
+        System.out.println("Byte  "+bt);
+        System.out.println("Char  "+letter);  // '/u0000'  '/uFFFF'
+        System.out.println("Short  "+sh);
+        System.out.println("Int  "+in);
+        System.out.println("LOng  "+lo);
+        System.out.println("Float  "+fl);
+        System.out.println("Double  "+dl);
+        System.out.println("Strig  "+s);
+    }
+
+    }
+
+public class DataType{
+	public static void main(String[] args) {
+
+		Data d1=new Data();
+
+		d1.displayData();
+	
+	}
+}
