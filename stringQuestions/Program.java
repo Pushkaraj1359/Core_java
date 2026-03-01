3c7da5d71386cc56b22ad96cb226740852298be4
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2026-03-01 08:14:31 +0530

    string questions

--- stringQuestions/Program.java
+++ stringQuestions/Program.java
@@ -6,3 +6,28 @@
 
 
 
+
+package stringQuestions;
+
+public class Program {
+    public static void main(String[] args) {
+        // method 01
+        String name = "Pushkaraj";
+        StringBuilder sb=new StringBuilder(name);
+
+        char first = sb.charAt(0);
+        char last =  sb.charAt(sb.length()-1);
+        String middle = sb.substring(1,sb.length()-1);   // subString method 
+        String result = last + middle + first;
+        System.out.println(result);
+    }
+}
+
+
+
+// sb.setCharAt(index, newCharacter);
+
+
+// substring(start, end)
+// ✔ Start index → Included
+// ❌ End index → Not Included
