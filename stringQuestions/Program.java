75eca8786dfaab60f4b856653973f2f0072b78a6
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2026-03-01 08:48:04 +0530

    string question and answers

--- stringQuestions/Program.java
+++ stringQuestions/Program.java
@@ -12,22 +12,54 @@ package stringQuestions;
 public class Program {
     public static void main(String[] args) {
         // method 01
-        String name = "Pushkaraj";
-        StringBuilder sb=new StringBuilder(name);
+            // String name = "Pushkaraj";
+            // StringBuilder sb=new StringBuilder(name);
 
-        char first = sb.charAt(0);
-        char last =  sb.charAt(sb.length()-1);
-        String middle = sb.substring(1,sb.length()-1);   // subString method 
-        String result = last + middle + first;
-        System.out.println(result);
+            // char first = sb.charAt(0);
+            // char last =  sb.charAt(sb.length()-1);
+            // String middle = sb.substring(1,sb.length()-1);   // subString method 
+            // String result = last + middle + first;
+            // System.out.println(result);
+    
+        // Method 02
+
+            // String name = "Pushkaraj";
+
+            // String first = name.substring(0,1);
+            // String last  = name.substring(name.length()-1);
+            // String middle = name.substring(1,name.length()-1);
+
+            // String result = last + middle + first;
+            // System.out.println(result);
+
+        // Method 03
+
+            String name = "Pushkaraj";
+
+            char first = name.charAt(0);
+            char last = name.charAt(name.length()-1);
+
+            String result = "";
+
+            result = result + last;
+
+            for(int i=1;i<name.length()-1;i++){
+                result = result + name.charAt(i);
+            }
+
+            result = result + first;
+            System.out.println(result);
+
     }
 }
 
 
 
 // sb.setCharAt(index, newCharacter);
+// It does NOT insert and delete
+// It only changes the existing character.
 
 
 // substring(start, end)
-// ✔ Start index → Included
-// ❌ End index → Not Included
+// Start index → Included
+// End index → Not Included
---
+++ stringQuestions/Program04.java
@@ -0,0 +1,42 @@
+// Q4. Count Vowels in a String
+
+// Problem: Count the number of vowels in the string.
+// Input: Education
+
+
+package stringQuestions;
+
+public class Program04 {
+    public static void main(String[] args) {
+
+    // Method 01
+        // String str="Education";
+        // int count = 0;
+
+        // for(int i=0;i<str.length();i++){
+        //     char ch = str.charAt(i);
+        //     if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || 
+        //         ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'){
+        //             count++;
+        //     }
+        // }
+        // System.out.println("Vowels count : "+count);
+
+
+    // Method 02 
+            String str = "Education";
+            StringBuilder sb = new StringBuilder(str);
+    
+            int count = 0;
+    
+            for(int i=0;i<sb.length();i++){
+                char ch = sb.charAt(i);
+                if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || 
+                    ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'){
+                        count++;
+                }
+            }
+            System.out.println("Vowels count : "+count);
+    }
+
+}
