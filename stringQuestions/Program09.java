48a266b9e1be1f1bd179c4e836d9282590c8c91d
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2026-03-01 18:43:36 +0530

    string problem and answers

--- stringQuestions/Program09.java
+++ stringQuestions/Program09.java
@@ -8,6 +8,54 @@ package stringQuestions;
 
 public class Program09 {
     public static void main(String[] args) {
-        String str="hello i am java developer";
+
+    //Method 01
+        // String str = "Java is very powerful language";
+        // str = str.trim();
+
+        // if(str.isEmpty()){
+        //     System.out.println("Word count: 0");
+        // } else {
+        //     // str.split("\\s+") splits the string into words by using one or more whitespace characters as the separator.
+        //     String[] words = str.split("\\s+");
+        //     System.out.println("Word count: " + words.length);
+        // }
+
+    //Method 02
+        // String str = "Java is very powerful language";
+
+        // int count = 0;
+        // boolean inWord = false;
+
+        // for(int i = 0; i < str.length(); i++){
+
+        //     if(str.charAt(i) != ' ' && inWord == false){
+        //         count++;
+        //         inWord = true;
+        //     }
+
+        //     else if(str.charAt(i) == ' '){
+        //         inWord = false;
+        //     }
+        // }
+
+        // System.out.println("Word count: " + count);
+
+
+        String str = "java is an powerful language";
+
+        int count = 0;
+        boolean inWord = false;
+
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i) != ' ' && inWord == false){
+                count++;
+                inWord=true;
+            }
+            else if(str.charAt(i) == ' '){
+                inWord = false;
+            }
+        }
+        System.out.println(count);
     }
 }
