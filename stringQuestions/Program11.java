8fca682c491d84bc3090c9f24d9053820db81efb
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2026-03-02 08:34:20 +0530

    string sort string by itsf length

---
+++ stringQuestions/Program11.java
@@ -0,0 +1,45 @@
+// Q11. Sort Words Lexicographically
+
+// Problem: Sort all words in dictionary (alphabetical) order.
+// Input: hello i am java developer
+// Output: am developer hello i java 
+
+
+
+package stringQuestions;
+
+public class Program11 {
+    public static void main(String[] args) {
+        // String str = "hello i am java developer";
+        // String[] words = str.split(" ");
+
+        // for(int i=1;i<words.length;i++){
+        //     for(int j=i+1;j<words.length;j++){
+        //         if(words[i].compareTo(words[j]) > 0){
+        //             String temp = words[i];
+        //             words[i]=words[j];
+        //             words[j]=temp;
+        //         }
+        //     }
+        // }
+        // for(String word : words){
+        //     System.out.println(word+" ");
+        // }
+    }
+}
+
+
+
+
+/*
+Lexicographical order means arranging strings in dictionary order by comparing
+characters based on their ASCII or Unicode values.
+Very Simple Meaning
+Lexicographically = Dictionary order (A to Z)
+
+Comparison happens:
+    Character by character
+    Based on ASCII value
+    Stops when difference is found
+
+ */
---
+++ stringQuestions/Program12.java
@@ -0,0 +1,32 @@
+/*Sort Words by Length
+
+Problem: Sort words based on their length (shortest to longest).
+Input: hello i am java developer
+Output: i am java hello developer */
+
+
+
+
+package stringQuestions;
+
+public class Program12 {
+    public static void main(String[] args) {
+
+        String str = "i am java hello developer";
+        String[] words = str.split(" ");
+
+        for(int i=0;i>words.length;i++){
+            for(int j=i+1;j>words.length;j++){
+                if(words[i].length()>words[j].length()){
+                    String temp = words[i];
+                    words[i]=words[j];
+                    words[j]=temp;
+                }
+            }
+        }
+        for(String word :  words){
+            System.out.println( word + " : "+word.length());
+        }
+
+    }
+}
