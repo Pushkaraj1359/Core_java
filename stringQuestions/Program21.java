b90e9ce2827dfc317f6994279a7f010d879ab37b
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2026-03-04 23:37:41 +0530

    string assignment

--- stringQuestions/Program21.java
+++ stringQuestions/Program21.java
@@ -1,5 +1,27 @@
+
+// Q21. Capitalize First Letter of Each Word
+
+// Problem: Capitalize the first letter of every word.
+// Input: hello i am java developer
+// Output: Hello I Am Java Developer
+
 package stringQuestions;
 
 public class Program21 {
-    
+    public static void main(String[] args){
+        String str = "hello i am java developer";
+        String[] arr = str.split(" ");
+        String result = "";
+
+        for(int i = 0; i < arr.length; i++){
+            String word = arr[i];
+
+            char firstChar = Character.toUpperCase(word.charAt(0));
+            String rest = word.substring(1);
+
+            result = result + firstChar + rest + " ";
+        }
+
+        System.out.println(result);
+    }
 }
