3ae2ebae67504ed54fc76328ed3e7e1e594c05cf
pushkaraj kulkarni <kulkarnip1359@gmail.com>
2026-03-08 15:33:16 +0530

    separate 0 and 1

---
+++ stringQuestions/Program26.java
@@ -0,0 +1,24 @@
+package stringQuestions;
+
+public class Program26 {
+    public static void main(String[] args) {
+        String str  = " 10101010";
+        char[] ch = str.toCharArray();
+        String result = "";
+        String zeros = "";
+
+        for(int i=0;i<ch.length;i++){
+
+            char character = ch[i];
+            if(character == '0'){
+                zeros = zeros + '0'; 
+            }
+            else{
+                result = result + character;
+            }
+        }
+        result = result + zeros;
+        System.out.println(result);
+
+    }
+}
